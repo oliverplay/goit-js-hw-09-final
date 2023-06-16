35d8c436b75da3ad41b03784f98638e96041eb69
oliverplay <116948332+oliverplay@users.noreply.github.com>
2023-06-16 05:24:44 +0000

    Deploying to gh-pages from @ oliverplay/goit-js-hw-09-final@35c22ff8e35f8e0e20faebda618e619b84e70d43 🚀

---
+++ 01-color-switcher.24e3c1e0.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=null;e.disabled=!0,t.addEventListener("click",(function(){a=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.disabled=!0,e.disabled=!1}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.24e3c1e0.js.map
---
+++ 01-color-switcher.ce1999b5.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;t.disabled=!0,e.addEventListener("click",(()=>{d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,e.disabled=!0,t.disabled=!1}),1e3)})),t.addEventListener("click",(()=>{clearInterval(d),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.ce1999b5.js.map
